286df48e8ce247fbd321113837a6e22e90ffea53
ejjoj <szymaskibartosz@gmail.com>
2020-08-21 17:50:47 +0200

    had to rename main app file

--- cantor.js
+++
@@ -1,9 +0,0 @@
-class ExchangeCounter extends React.Component {
-  render() {
-    return (
-      <div>Kantor</div>
-    );
-  }
-}
-
-ReactDOM.render(<ExchangeCounter />, document.getElementById('root'));
